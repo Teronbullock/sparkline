d10661363bf22b93b573df6abb9fffda384ebb26
Teron Bullock <teron.bullock@gmail.com>
2025-03-02 15:11:23 -0500

    add setup files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,98 @@
-import Image from "next/image";
+import Btn from '@components/btn/Btn';
+import { Column, Columns } from '@components/Columns/Columns';
+import Hero from '@components/Hero/Hero';
+import TwoColImgSection from '@components/Two-col-img-section/Two-col-img-section';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className=''>
+      <main className=''>
+        <Hero
+          type='FrontPage'
+          bgImgLabel="Hero Section with background image. Image of a person working out. This person's back is facing the camera, while they hold a bar with weights above their head. Photo by John Arano on Unsplash."
+          contentClass='container px-4 py-14 mx-auto'
+        >
+          <Columns colsClasses='flex-col lg:flex-row'>
+            <Column colClasses='lg:pe-8 xl:pe-10'>
+              <h1 className='pb-6 text-4xl font-black lg:pb-0 xl:text-5xl mt-0'>
+                Track your fitness and reach your goals
+              </h1>
+            </Column>
+            <Column colClasses=''>
+              <p className='mb-8 text-2xl'>
+                With our exercise tracking app, you can easily entry your
+                exercises and stay motivated to reach your goals. Weather your a
+                beginner or a seasoned pro, we have the tools to help you
+                succeed.
+              </p>
+              <div className='flex'>
+                <Btn
+                  btnclass='mr-8 bg-red-600 text-white py-4 px-8 hover:text-black hover:bg-transparent hover:border-red-600 hover:border-2'
+                  text='Sign Up'
+                  link='/register'
+                />
+                <Btn
+                  text='Learn More'
+                  link='/about'
+                  btnclass='bg-gray-900 text-white py-4 px-8 hover:text-black hover:bg-transparent hover:border-gray-900 hover:border-2'
+                />
+              </div>
+            </Column>
+          </Columns>
+        </Hero>
+        <section className='sl-section bg-sky-600 py-32 text-white'>
+          <Columns colsClasses='container mx-auto'>
+            <Column>
+              <div>
+                <p className='text-center xl:text-5xl'>
+                  “Work hard in silence, let your success be your noise.”
+                </p>
+                <p className='mt-8 text-center xl:text-4xl'>Anonymous</p>
+              </div>
+            </Column>
+          </Columns>
+        </section>
+        <TwoColImgSection
+          sectionClasses='lg:pt-24 pb-12'
+          leftColContent={
+            <img src='https://via.placeholder.com/800x600' alt='placeholder' />
+          }
+          rightColContent={
+            <div className='p-12 pb-0 pe-0 ps-12 xl:pt-0'>
+              <h2 className='mb-6 text-4xl font-black'>
+                Track Your Exercises and Stay Motivated with Our App
+              </h2>
+              <p className='text-2xl'>
+                Our exercise-tracking app allows you to easily select or enter
+                your exercises and track your progress.
+              </p>
+            </div>
+          }
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <TwoColImgSection
+          sectionClasses='lg:pt-12 pb-24'
+          imgPlacement='right'
+          leftColContent={
+            <div className='p-12 pb-0 pe-12 ps-0 xl:pt-0'>
+              <h2 className='mb-6 text-4xl font-black'>
+                Set and Track Your Fitness Goals
+              </h2>
+              <p className='text-2xl'>
+                Our app provides a comprehensive goal setting and tracking
+                system to help you stay motivated and achieve your fitness
+                goals. Whether you're a beginner or an experienced athlete, our
+                app has the tools you need to succeed. With features like
+                exercise selection, progress tracking, and personalized
+                recommendations, you'll be able to take your fitness journey to
+                the next level.
+              </p>
+            </div>
+          }
+          rightColContent={
+            <img src='https://via.placeholder.com/800x600' alt='placeholder' />
+          }
+        />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
